760d6cb57b9c0778143cc84e58995bc9193201d8
Chito7 <luisrt720@gmail.com>
2024-04-28 18:58:49 -0700

    Analyze capture script
    Created a scrip to sort through the packets.

---
+++ analyzeCaps.py
@@ -0,0 +1,17 @@
+import pyshark
+
+#Set the path to the pcap file I want to analyze
+capture = pyshark.FileCapture('./WireSharkCaps/trialone.pcapng')
+
+pkt_num = 0
+
+#display packets in the capture file
+for packet in capture:
+    pkt_num += 1
+    if 'dns' in packet:
+        hostname = packet.dns.qry_name
+        if hostname and 'tiktok.com' in hostname:
+            print(f"\n\nPACKET {pkt_num} with hostname tiktok found!\n\n")
+            print(packet)
+
+
